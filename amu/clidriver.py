f49d922efab3533f394b51bc2478e5464b0559bf
Chris O'Neil <chris.oneil@gmail.com>
2014-12-30 05:25:40 +0000

    Fix some linting

--- amu/clidriver.py
+++ amu/clidriver.py
@@ -8,17 +8,26 @@ def main():
     return driver.main()
 
 class CliDriver(object):
-    def main(self):
+    def _get_arguments(self):
         parser = argparse.ArgumentParser()
         subparsers = parser.add_subparsers(dest='command')
         subparsers.add_parser('rip', help='rips the current CD to WAV')
-        search_parser = subparsers.add_parser('search', prog='search', help='performs a very simple search on discogs')
+        search_parser = subparsers.add_parser(
+            'search',
+            prog='search',
+            help='performs a very simple search on discogs')
         search_parser.add_argument('term', help='the term to search for')
-        encode_parser = subparsers.add_parser('encode', help='encodes a CD or a set of WAV files to mp3')
-        encode_parser.add_argument('source', choices=['cd', 'wav'], help='the source to encode from')
-        encode_parser.add_argument('releaseid', type=int, help='the ID of the discogs release')
-        args = parser.parse_args()
-        print(args)
+        encode_parser = subparsers.add_parser(
+            'encode', help='encodes a CD or a set of WAV files to mp3')
+        encode_parser.add_argument(
+            'source', choices=['cd', 'wav'], help='the source to encode from')
+        encode_parser.add_argument(
+            'releaseid', type=int, help='the ID of the discogs release')
+        return parser.parse_args()
+
+    def main(self):
+        """ The main entry point for the CLI driver """
+        self._get_arguments()
         return 0
 
 if __name__ == '__main__':
