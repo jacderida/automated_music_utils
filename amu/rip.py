274902e703cdb4221ba2f9ca1a84e71d23e4082b
Chris O'Neil <chris.oneil@gmail.com>
2015-01-06 15:07:11 +0000

    Get ruby ripper path from environment variable

--- amu/rip.py
+++ amu/rip.py
@@ -14,6 +14,10 @@ class RubyRipperCdRipper(object):
         if not subprocess.call(['which', 'rubyripper_cli']):
             self._rubyripper_path = 'rubyripper_cli'
             return True
+        path_from_env_variable = os.environ.get('RUBYRIPPER_CLI_PATH')
+        if path_from_env_variable:
+            self._rubyripper_path = path_from_env_variable
+            return True
         return False
 
     def rip_cd(self):
--- tests/unit/test_rubyripper_cd_ripper.py
+++ tests/unit/test_rubyripper_cd_ripper.py
@@ -13,3 +13,16 @@ class RubyRipperCdRipperTest(unittest.TestCase):
             self.assertTrue(result)
             self.assertEqual('rubyripper_cli', ripper.rubyripper_path)
             mock.assert_called_with(['which', 'rubyripper_cli'])
+
+    def test_is_installed_ruby_ripper_path_is_set_on_environment_variable_correctly_set(self):
+        ripper = RubyRipperCdRipper()
+        with patch('amu.rip.subprocess.call') as subprocess_mock:
+            with patch('amu.rip.os.environ') as environ_mock:
+                subprocess_mock.return_value = 1
+                environ_mock.get.return_value = \
+                    '/opt/rubyripper/rubyripper_cli.rb'
+                result = ripper.is_installed()
+                self.assertTrue(result)
+                self.assertEqual(
+                    '/opt/rubyripper/rubyripper_cli.rb', ripper.rubyripper_path)
+                environ_mock.get.assert_called_with('RUBYRIPPER_CLI_PATH')
