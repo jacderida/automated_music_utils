2816efdb764decb39e2086d5cf47e3dd3785d582
Chris O'Neil <chris.oneil@gmail.com>
2015-01-12 10:03:16 +0000

    Remove is_installed method
    The ConfigurationProvider class now handles this.

--- amu/rip.py
+++ amu/rip.py
@@ -11,23 +11,6 @@ class RubyRipperCdRipper(object):
     def rubyripper_path(self):
         return self._rubyripper_path
 
-    def is_installed(self):
-        if not subprocess.call(['which', 'rubyripper_cli']):
-            self._rubyripper_path = 'rubyripper_cli'
-            return True
-        path_from_env_variable = os.environ.get('RUBYRIPPER_CLI_PATH')
-        if path_from_env_variable:
-            self._rubyripper_path = path_from_env_variable
-            return True
-        config = ConfigParser.ConfigParser()
-        config_path = os.path.join(os.path.expanduser('~'), '.amu_config')
-        config.read(config_path)
-        path_from_config = config.get('ripper', 'path')
-        if path_from_config:
-            self._rubyripper_path = path_from_config
-            return True
-        return False
-
     def rip_cd(self):
         popen = subprocess.Popen(
             [self.rubyripper_path, '-d'], stdout=subprocess.PIPE)
