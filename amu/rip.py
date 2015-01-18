6be6fa3c04318965b48c16f6c88789e5a4a65ead
Chris O'Neil <chris.oneil@gmail.com>
2015-01-18 23:13:51 +0000

    Case for ripping destination being replaced
    For this case the destination is hard coded. This will most likely
    change to a randomly generated destination.

--- amu/rip.py
+++ amu/rip.py
@@ -1,5 +1,6 @@
 import ConfigParser
 import os
+import re
 import subprocess
 from amu.config import ConfigurationProvider
 
@@ -11,11 +12,17 @@ class RubyRipperCdRipper(object):
         self._config_provider = config_provider
 
     def rip_cd(self):
+        config_path = self._config_provider.get_ruby_ripper_config_file()
+        with open(config_path, 'r') as config_file:
+            lines = config_file.readlines()
+        with open(config_path, 'w') as config_file:
+            for line in lines:
+                config_file.write(re.sub('REPLACE_BASE_DIR', '/tmp/rip', line))
         subprocess_args = [
             self._config_provider.get_ruby_ripper_path(),
             '--defaults',
             '--file',
-            self._config_provider.get_ruby_ripper_config_file()
+            config_path
         ]
         popen = subprocess.Popen(subprocess_args, stdout=subprocess.PIPE)
         lines_iterator = iter(popen.stdout.readline, "")
--- tests/unit/test_rubyripper_cd_ripper.py
+++ tests/unit/test_rubyripper_cd_ripper.py
@@ -1,13 +1,16 @@
 import mock
 import subprocess
 import unittest
+from mock import MagicMock
 from amu.rip import RubyRipperCdRipper
 
 
 class RubyRipperCdRipperTest(unittest.TestCase):
+    @mock.patch('amu.rip.open', create=True)
     @mock.patch('amu.rip.ConfigurationProvider', autospec=True)
     @mock.patch('amu.rip.subprocess.Popen')
-    def test__rip_cd__call_ruby_ripper_with_correct_args__ruby_ripper_is_called_with_correct_args(self, subprocess_mock, config_mock):
+    def test__rip_cd__call_ruby_ripper_with_correct_args__ruby_ripper_is_called_with_correct_args(self, subprocess_mock, config_mock, open_mock):
+        open_mock.return_value = MagicMock(spec=file)
         config_mock.get_ruby_ripper_path.return_value = \
             '/opt/rubyripper/rubyripper_cli'
         config_mock.get_ruby_ripper_config_file.return_value = \
@@ -24,3 +27,27 @@ class RubyRipperCdRipperTest(unittest.TestCase):
             '/opt/rubyripper/config_file'
         ]
         subprocess_mock.assert_called_with(subprocess_args, stdout=subprocess.PIPE)
+
+    @mock.patch('amu.rip.re.sub')
+    @mock.patch('amu.rip.open', create=True)
+    @mock.patch('amu.rip.ConfigurationProvider', autospec=True)
+    @mock.patch('amu.rip.subprocess.Popen')
+    def test__rip_cd__replace_output_destination_in_config_file__destination_is_replaced_in_config_file(self, subprocess_mock, config_mock, open_mock, sub_mock):
+        sample_file_contents = [
+            'mp3=false',
+            'cdrom=/dev/cdrom',
+            'basedir=REPLACE_BASE_DIR'
+        ]
+        open_mock.return_value = MagicMock(spec=file)
+        file_handle = open_mock.return_value.__enter__.return_value
+        file_handle.readlines.return_value = sample_file_contents
+        config_mock.get_ruby_ripper_path.return_value = \
+            '/opt/rubyripper/rubyripper_cli'
+        config_mock.get_ruby_ripper_config_file.return_value = \
+            '/opt/rubyripper/config_file'
+        process_mock = mock.Mock()
+        process_mock.stdout.readline = lambda: ""
+        subprocess_mock.return_value = process_mock
+        ripper = RubyRipperCdRipper(config_mock)
+        ripper.rip_cd()
+        sub_mock.assert_called_with('REPLACE_BASE_DIR', '/tmp/rip', 'basedir=REPLACE_BASE_DIR')
