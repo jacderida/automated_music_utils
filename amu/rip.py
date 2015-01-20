dcd3907e8f4031140b8b9d137d6856dd71dafe3f
Chris O'Neil <chris.oneil@gmail.com>
2015-01-20 23:04:06 +0000

    Remove irrelevant test and update existing case
    Update it to use the method on the config file provider that hides the
    nasty details of replacing the config file. Further cases to follow.

--- amu/rip.py
+++ amu/rip.py
@@ -12,17 +12,11 @@ class RubyRipperCdRipper(object):
         self._config_provider = config_provider
 
     def rip_cd(self):
-        config_path = self._config_provider.get_ruby_ripper_config_file()
-        with open(config_path, 'r') as config_file:
-            lines = config_file.readlines()
-        with open(config_path, 'w') as config_file:
-            for line in lines:
-                config_file.write(re.sub('REPLACE_BASE_DIR', '/tmp/rip', line))
         subprocess_args = [
             self._config_provider.get_ruby_ripper_path(),
             '--defaults',
             '--file',
-            config_path
+            self._config_provider.get_temp_config_file_for_ripper('destination')
         ]
         popen = subprocess.Popen(subprocess_args, stdout=subprocess.PIPE)
         lines_iterator = iter(popen.stdout.readline, "")
