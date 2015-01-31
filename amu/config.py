3a180a0b1bc809eec8e14a2f95b18f14edbead37
Chris O'Neil <chris.oneil@gmail.com>
2015-01-31 21:15:33 +0000

    Refactor to remove duplication

--- amu/config.py
+++ amu/config.py
@@ -17,28 +17,18 @@ class ConfigurationProvider(object):
             return 'lame'
         path_from_env_variable = os.environ.get('LAME_PATH')
         if path_from_env_variable:
-            return self._get_verified_path_from_environment_variable(path_from_env_variable)
-        config = ConfigParser.ConfigParser()
-        config_path = os.path.join(os.path.expanduser('~'), '.amu_config')
-        if not os.path.exists(config_path):
-            raise ConfigurationError(
-                'The .amu_config file does not exist in your home directory.')
-        config.read(config_path)
-        path_from_config = config.get('encoder', 'path')
-        if not os.path.exists(path_from_config):
-            raise ConfigurationError(
-                """The path specified to ruby ripper in the .amu_config
-                file is incorrect. Please provide a valid path for
-                ruby ripper.""")
-        return path_from_config
+            return self._get_verified_path_from_environment_variable(
+                path_from_env_variable, 'LAME_PATH', 'lame')
+        return self._get_verified_path_from_config_file('encoder', 'lame')
 
     def get_ruby_ripper_path(self):
         if not subprocess.call(['which', 'rubyripper_cli']):
             return 'rubyripper_cli'
         path_from_env_variable = os.environ.get('RUBYRIPPER_CLI_PATH')
         if path_from_env_variable:
-            return self._get_verified_path_from_environment_variable(path_from_env_variable)
-        return self._get_verified_path_from_config_file()
+            return self._get_verified_path_from_environment_variable(
+                path_from_env_variable, 'RUBYRIPPER_CLI_PATH', 'ruby ripper')
+        return self._get_verified_path_from_config_file('ripper', 'ruby ripper')
 
     def get_ruby_ripper_config_file(self):
         path_from_env_variable = os.environ.get('RUBYRIPPER_CONFIG_PATH')
@@ -61,25 +51,25 @@ class ConfigurationProvider(object):
                 config_file.write(re.sub('REPLACE_BASE_DIR', destination_path, line))
         return temp_path
 
-    def _get_verified_path_from_environment_variable(self, path_from_env_variable):
+    def _get_verified_path_from_environment_variable(self, path_from_env_variable, env_variable_name, program):
         if not os.path.exists(path_from_env_variable):
             raise ConfigurationError(
-                """The path specified by RUBYRIPPER_CLI_PATH
+                """The path specified by {0}
                 is incorrect. Please provide a valid path for
-                ruby ripper.""")
+                {1}.""".format(env_variable_name, program))
         return path_from_env_variable
 
-    def _get_verified_path_from_config_file(self):
+    def _get_verified_path_from_config_file(self, config_section, program):
         config = ConfigParser.ConfigParser()
         config_path = os.path.join(os.path.expanduser('~'), '.amu_config')
         if not os.path.exists(config_path):
             raise ConfigurationError(
                 'The .amu_config file does not exist in your home directory.')
         config.read(config_path)
-        path_from_config = config.get('ripper', 'path')
+        path_from_config = config.get(config_section, 'path')
         if not os.path.exists(path_from_config):
             raise ConfigurationError(
-                """The path specified to ruby ripper in the .amu_config
+                """The path specified to {0} in the .amu_config
                 file is incorrect. Please provide a valid path for
-                ruby ripper.""")
+                {0}.""".format(program))
         return path_from_config
