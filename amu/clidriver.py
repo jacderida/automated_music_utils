1c30393a878a0ed306bc88557405a282011ce927
Chris O'Neil <chris.oneil@gmail.com>
2015-02-01 00:49:08 +0000

    Add arguments for encoding wav

--- amu/clidriver.py
+++ amu/clidriver.py
@@ -25,11 +25,15 @@ class CliDriver(object):
             help='performs a very simple search on discogs')
         search_parser.add_argument('term', help='the term to search for')
         encode_parser = subparsers.add_parser(
-            'encode', help='encodes a CD or a set of WAV files to mp3')
+            'encode', help='Encodes a CD or a set of WAV files to mp3.')
         encode_parser.add_argument(
-            'source', choices=['cd', 'wav'], help='the source to encode from')
+            'encoding_from', choices=['cd', 'wav'], help='The source to encode from.')
         encode_parser.add_argument(
-            'releaseid', type=int, help='the ID of the discogs release')
+            'encoding_to', choices=['mp3', 'flac'], help='The destination to encode to.')
+        encode_parser.add_argument(
+            '--source', help='The destination of the source wav file. This can be a file or directory.')
+        encode_parser.add_argument(
+            '--destination', help='The destination of the resulting mp3 or flac. This can be a file or directory.')
         return parser
 
     def _get_arguments(self):
