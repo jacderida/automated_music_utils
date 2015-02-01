acaa5f7da3711e3b59fa66d21fdbbf86b7d82e85
Chris O'Neil <chris.oneil@gmail.com>
2015-02-01 02:00:59 +0000

    Case for default value of keep source

--- tests/unit/test_command_parser.py
+++ tests/unit/test_command_parser.py
@@ -108,3 +108,14 @@ class CommandParserTest(unittest.TestCase):
         parser = CommandParser(config_mock, cd_ripper_mock, encoder_mock)
         command = parser.from_args(args)
         self.assertTrue(command.keep_source)
+
+    @mock.patch('amu.encode.LameEncoder')
+    @mock.patch('amu.config.ConfigurationProvider')
+    @mock.patch('amu.rip.RubyRipperCdRipper')
+    def test__from_args__encode_wav_to_mp3_command_without_keep_source_specified__keep_source_should_be_false(self, config_mock, cd_ripper_mock, encoder_mock):
+        driver = CliDriver()
+        arg_parser = driver.get_argument_parser()
+        args = arg_parser.parse_args(['encode', 'wav', 'mp3'])
+        parser = CommandParser(config_mock, cd_ripper_mock, encoder_mock)
+        command = parser.from_args(args)
+        self.assertFalse(command.keep_source)
