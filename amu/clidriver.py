0cb640596e0f9170a138c28b325b1af8e28a146b
Chris O'Neil <chris.oneil@gmail.com>
2015-02-07 22:49:05 +0000

    Move parsing into its own module

--- amu/clidriver.py
+++ amu/clidriver.py
@@ -2,8 +2,7 @@
 import argparse
 import os
 import sys
-from amu.commands.ripcdcommand import RipCdCommand
-from amu.commands.encodewavtomp3command import EncodeWavToMp3Command
+from amu.parsing import CommandParser
 from amu.config import ConfigurationProvider
 from amu.rip import RubyRipperCdRipper
 from amu.encode import LameEncoder
@@ -53,44 +52,6 @@ class CliDriver(object):
         command.validate()
         command.execute()
         return 0
-
-class CommandParser(object):
-    """ Responsible for parsing the string based command from the command line
-        into a command object that can be executed.
-    """
-    def __init__(self, configuration_provider, cd_ripper, encoder):
-        self._configuration_provider = configuration_provider
-        self._cd_ripper = cd_ripper
-        self._encoder = encoder
-
-    def from_args(self, args):
-        if args.command == 'rip':
-            return self._get_rip_command(args)
-        elif args.command == 'encode':
-            return self._get_encode_command(args)
-
-    def _get_rip_command(self, args):
-        command = RipCdCommand(self._configuration_provider, self._cd_ripper)
-        if args.destination:
-            command.destination = args.destination
-        else:
-            command.destination = os.getcwd()
-        return command
-
-    def _get_encode_command(self, args):
-        if args.encoding_from == 'wav' and args.encoding_to == 'mp3':
-            command = EncodeWavToMp3Command(self._configuration_provider, self._encoder)
-            if args.source:
-                command.source = args.source
-            else:
-                command.source = os.getcwd()
-            if args.destination:
-                command.destination = args.destination
-            else:
-                command.destination = os.getcwd()
-            if args.keep_source:
-                command.keep_source = True
-            return command
 
 if __name__ == '__main__':
     sys.exit(main())
