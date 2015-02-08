28a8b25fcf89952417e6da59db1ae47e4ceb5735
Chris O'Neil <chris.oneil@gmail.com>
2015-02-08 15:54:30 +0000

    Mock is file call

--- tests/unit/test_encode_command_parser.py
+++ tests/unit/test_encode_command_parser.py
@@ -21,11 +21,10 @@ class EncodeCommandParserTest(unittest.TestCase):
         self.assertEqual('/some/path/to/song.mp3', command.destination)
 
     @mock.patch('os.path.exists')
-    @mock.patch('os.path.isfile')
     @mock.patch('amu.encode.LameEncoder')
     @mock.patch('amu.config.ConfigurationProvider')
     @mock.patch('amu.rip.RubyRipperCdRipper')
-    def test__parse_wav_to_mp3__source_does_not_exist__throws_command_parsing_exception(self, config_mock, cd_ripper_mock, encoder_mock, isfile_mock, exists_mock):
+    def test__parse_wav_to_mp3__source_does_not_exist__throws_command_parsing_exception(self, config_mock, cd_ripper_mock, encoder_mock, exists_mock):
         exists_mock.return_value = False
         parser = EncodeCommandParser(config_mock, cd_ripper_mock, encoder_mock)
         with self.assertRaisesRegexp(CommandParsingError, 'The source directory or wav file must exist'):
