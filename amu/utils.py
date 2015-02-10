33e455955a0af572b22b669e599f991554c97739
Chris O'Neil <chris.oneil@gmail.com>
2015-02-10 23:13:06 +0000

    Add util function to get number of tracks on CD

--- amu/utils.py
+++ amu/utils.py
@@ -2,6 +2,7 @@
 import re
 import os
 import shutil
+import subprocess
 from copy import deepcopy
 from mock import DEFAULT, Mock
 
@@ -30,3 +31,10 @@ def get_mock_with_stored_call_args(mock):
         return DEFAULT
     mock.side_effect = side_effect
     return new_mock
+
+def get_number_of_tracks_on_cd():
+    output = subprocess.check_output(['cdparanoia', '-sQ'], stderr=subprocess.STDOUT)
+    lines = [line.strip() for line in output.splitlines() if line.strip()]
+    track_lines = [m.group(0) for line in lines for m in [re.search('^[0-9].*', line)] if m]
+    return len(track_lines)
+
