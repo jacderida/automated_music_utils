da41072f8ad8eb8bef2c9d6750a6cb0fb2449ddc
Chris O'Neil <chris.oneil@gmail.com>
2015-07-12 21:07:01 +0100

    Case for title with space

--- tests/unit/test_add_tag_command_args.py
+++ tests/unit/test_add_tag_command_args.py
@@ -135,3 +135,22 @@ class AddTagCommandArgsTest(unittest.TestCase):
         ])
         command_args = AddTagCommandArgs.from_args(args)
         self.assertEqual('Pimpshifter', command_args.title)
+
+    def test__from_args__when_the_title_has_a_space_in_the_name__the_title_should_be_specified_without_surrounding_quotes(self):
+        driver = CliDriver()
+        arg_parser = driver.get_argument_parser()
+        args = arg_parser.parse_args([
+            'tag',
+            'add',
+            'mp3',
+            '--source=/some/path/to/song.mp3',
+            '--artist=Legowelt',
+            '--album=Pimpshifter',
+            '--title="Geneva Hideout"',
+            '--year=2000',
+            '--genre=Electronic',
+            '--track-number=2',
+            '--track-total=6'
+        ])
+        command_args = AddTagCommandArgs.from_args(args)
+        self.assertEqual('Geneva Hideout', command_args.title)
