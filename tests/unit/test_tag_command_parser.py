1104ff8dbed21afff2f7a3e84208edf8cb812319
Chris O'Neil <chris.oneil@gmail.com>
2015-07-18 19:51:39 +0100

    Case for title set on tracks

--- tests/unit/test_tag_command_parser.py
+++ tests/unit/test_tag_command_parser.py
@@ -371,3 +371,69 @@ class TagCommandParserTest(unittest.TestCase):
         self.assertEqual('Legowelt', commands[3].artist)
         self.assertEqual('Legowelt', commands[4].artist)
         self.assertEqual('Legowelt', commands[5].artist)
+
+    @mock.patch('os.walk')
+    @mock.patch('amu.config.ConfigurationProvider')
+    def test__parse_from_release_model__release_has_single_artist_and_6_tracks__release_album_is_set_on_tracks(self, config_mock, walk_mock):
+        release_model = ReleaseModel()
+        release_model.artist = 'Legowelt'
+        release_model.title = 'Pimpshifter'
+        release_model.label = 'Bunker Records'
+        release_model.catno = 'BUNKER 3002'
+        release_model.format = 'Vinyl'
+        release_model.country = 'Netherlands'
+        release_model.year = '2000'
+        release_model.genre = 'Electronic'
+        release_model.style = 'Electro'
+        release_model.add_track_directly(None, 'Sturmvogel', 1)
+        release_model.add_track_directly(None, 'Geneva Hideout', 2)
+        release_model.add_track_directly(None, 'Ricky Ramjet', 3)
+        release_model.add_track_directly(None, 'Nuisance Lover', 4)
+        release_model.add_track_directly(None, 'Strange Girl', 5)
+        release_model.add_track_directly(None, 'Total Pussy Control', 6)
+        source_path = '/some/path/to/mp3s'
+        walk_mock.return_value = [
+            (source_path, (), ('01 - Track 1.mp3', '02 - Track 2.mp3', '03 - Track 3.mp3', '04 - Track 4.mp3', '05 - Track 5.mp3', '06 - Track 6.mp3')),
+        ]
+
+        parser = TagCommandParser(config_mock)
+        commands = parser.parse_from_release_model(source_path, release_model)
+        self.assertEqual('Pimpshifter', commands[0].album)
+        self.assertEqual('Pimpshifter', commands[1].album)
+        self.assertEqual('Pimpshifter', commands[2].album)
+        self.assertEqual('Pimpshifter', commands[3].album)
+        self.assertEqual('Pimpshifter', commands[4].album)
+        self.assertEqual('Pimpshifter', commands[5].album)
+
+    @mock.patch('os.walk')
+    @mock.patch('amu.config.ConfigurationProvider')
+    def test__parse_from_release_model__release_has_single_artist_and_6_tracks__title_is_set_on_tracks(self, config_mock, walk_mock):
+        release_model = ReleaseModel()
+        release_model.artist = 'Legowelt'
+        release_model.title = 'Pimpshifter'
+        release_model.label = 'Bunker Records'
+        release_model.catno = 'BUNKER 3002'
+        release_model.format = 'Vinyl'
+        release_model.country = 'Netherlands'
+        release_model.year = '2000'
+        release_model.genre = 'Electronic'
+        release_model.style = 'Electro'
+        release_model.add_track_directly(None, 'Sturmvogel', 1)
+        release_model.add_track_directly(None, 'Geneva Hideout', 2)
+        release_model.add_track_directly(None, 'Ricky Ramjet', 3)
+        release_model.add_track_directly(None, 'Nuisance Lover', 4)
+        release_model.add_track_directly(None, 'Strange Girl', 5)
+        release_model.add_track_directly(None, 'Total Pussy Control', 6)
+        source_path = '/some/path/to/mp3s'
+        walk_mock.return_value = [
+            (source_path, (), ('01 - Track 1.mp3', '02 - Track 2.mp3', '03 - Track 3.mp3', '04 - Track 4.mp3', '05 - Track 5.mp3', '06 - Track 6.mp3')),
+        ]
+
+        parser = TagCommandParser(config_mock)
+        commands = parser.parse_from_release_model(source_path, release_model)
+        self.assertEqual('Sturmvogel', commands[0].title)
+        self.assertEqual('Geneva Hideout', commands[1].title)
+        self.assertEqual('Ricky Ramjet', commands[2].title)
+        self.assertEqual('Nuisance Lover', commands[3].title)
+        self.assertEqual('Strange Girl', commands[4].title)
+        self.assertEqual('Total Pussy Control', commands[5].title)
