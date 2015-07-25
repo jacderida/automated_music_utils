f3b056ec506b668a5172245ce87082bcab745ea8
Chris O'Neil <chris.oneil@gmail.com>
2015-07-25 16:33:19 +0100

    Case for invalid ID
    For now, I think this is all the metadata service needs. Later on, it
    may have a search interface.

--- amu/metadata.py
+++ amu/metadata.py
@@ -1,10 +1,22 @@
 import discogs_client
+import sys
+from discogs_client.exceptions import HTTPError
 from amu.models import ReleaseModel
 
 
+class ReleaseNotFoundError(Exception):
+    def __init__(self, message):
+        super(ReleaseNotFoundError, self).__init__(message)
+        self.message = message
+
 class DiscogsMetadataService(object):
     def get_release_by_id(self, id):
-        client = discogs_client.Client('amu/0.1')
-        release = client.release(id)
-        release.refresh()
-        return ReleaseModel.from_discogs_release(release)
+        try:
+            client = discogs_client.Client('amu/0.1')
+            release = client.release(id)
+            release.refresh()
+            return ReleaseModel.from_discogs_release(release)
+        except HTTPError, ex:
+            if ex.status_code == 404:
+                raise ReleaseNotFoundError('There is no release with ID {0}.'.format(id))
+            raise ex
--- tests/integration/test_discogs_meta_data_service.py
+++ tests/integration/test_discogs_meta_data_service.py
@@ -1,5 +1,6 @@
 import unittest
 from amu.metadata import DiscogsMetadataService
+from amu.metadata import ReleaseNotFoundError
 from amu.models import ReleaseModel
 
 class DiscogsMetadataServiceTest(unittest.TestCase):
@@ -7,3 +8,8 @@ class DiscogsMetadataServiceTest(unittest.TestCase):
         service = DiscogsMetadataService()
         release = service.get_release_by_id(32662)
         self.assertEqual(32662, release.id)
+
+    def test__get_release_by_id__invalid_release_id__raises_release_not_found_error(self):
+        with self.assertRaisesRegexp(ReleaseNotFoundError, 'There is no release with ID invalid_id.'):
+            service = DiscogsMetadataService()
+            service.get_release_by_id('invalid_id')
