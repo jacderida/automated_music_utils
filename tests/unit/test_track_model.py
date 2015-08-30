f0ea1188c4f277608cabcc114fcc21e7848898bb
Chris O'Neil <chris.oneil@gmail.com>
2015-08-30 23:34:19 +0100

    Case for track with artist

--- tests/unit/test_track_model.py
+++ tests/unit/test_track_model.py
@@ -123,3 +123,17 @@ class TrackModelTest(unittest.TestCase):
             output = out.getvalue().strip()
             disc_info = output[-5:]
             self.assertEqual('20/20', disc_info)
+
+    def test__repr__track_with_artist__should_print_the_artist(self):
+        track_model = TrackModel()
+        track_model.artist = 'Aphex Twin'
+        track_model.title = 'Xtal'
+        track_model.track_number = 1
+        track_model.track_total = 10
+        track_model.disc_number = 1
+        track_model.disc_total = 1
+
+        with captured_output() as (out, _):
+            print track_model
+            output = out.getvalue().strip()
+            self.assertIn('Aphex Twin', output)
