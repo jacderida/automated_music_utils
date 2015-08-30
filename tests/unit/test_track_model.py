04a2169cb3d4c94034b74c6e6afc0d0c9842440a
Chris O'Neil <chris.oneil@gmail.com>
2015-08-30 21:21:40 +0100

    Extra case for non-padded track

--- tests/unit/test_track_model.py
+++ tests/unit/test_track_model.py
@@ -3,7 +3,7 @@ from amu.models import TrackModel
 from tests.helpers import captured_output
 
 class TrackModelTest(unittest.TestCase):
-    def test__repr__track_with_no_artist__should_print_the_track_number(self):
+    def test__repr__track_with_no_artist__should_print_the_padded_track_number(self):
         track_model = TrackModel()
         track_model.title = 'Xtal'
         track_model.track_number = 1
@@ -15,6 +15,19 @@ class TrackModelTest(unittest.TestCase):
             print track_model
             output = out.getvalue().strip()
             self.assertRegexpMatches(output, '^01.*')
+
+    def test__repr__track_with_no_artist__should_print_the_track_number(self):
+        track_model = TrackModel()
+        track_model.title = 'Xtal'
+        track_model.track_number = 10
+        track_model.track_total = 10
+        track_model.disc_number = 1
+        track_model.disc_total = 1
+
+        with captured_output() as (out, _):
+            print track_model
+            output = out.getvalue().strip()
+            self.assertRegexpMatches(output, '^10.*')
 
     def test__repr__track_with_no_artist__should_print_the_track_total(self):
         track_model = TrackModel()
