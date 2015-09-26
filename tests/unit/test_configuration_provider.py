e9320b1ddc61b5434301fc6196469413d7a0e086
Chris O'Neil <chris.oneil@gmail.com>
2015-09-26 22:21:54 +0100

    Case for non-existent config file

--- tests/unit/test_configuration_provider.py
+++ tests/unit/test_configuration_provider.py
@@ -566,3 +566,16 @@ class ConfigurationProviderTest(unittest.TestCase):
         config_get_mock.return_value = '/some/path/to/flac'
         config_provider.get_flac_path()
         config_read_mock.assert_called_with('/home/user/.amu_config')
+
+    @mock.patch('amu.config.os.path.exists')
+    @mock.patch('amu.config.os.path.expanduser')
+    @mock.patch('amu.config.os.environ')
+    @mock.patch('amu.config.subprocess.call')
+    def test__get_flac_path__config_file_does_not_exist__throws_configuration_error(self, subprocess_mock, environ_mock, expanduser_mock, path_exists_mock):
+        subprocess_mock.return_value = 1
+        environ_mock.get.return_value = None
+        expanduser_mock.return_value = '/home/user/'
+        path_exists_mock.return_value = False
+        with self.assertRaisesRegexp(ConfigurationError, 'The .amu_config file does not exist in your home directory.'):
+            config_provider = ConfigurationProvider(MaskReplacer())
+            config_provider.get_flac_path()
