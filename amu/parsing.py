6d11ad304425bf8e17b1f7034d35181f0af74413
Chris O'Neil <chris.oneil@gmail.com>
2015-10-11 00:25:44 +0100

    Case for source on move file commands

--- amu/parsing.py
+++ amu/parsing.py
@@ -630,10 +630,12 @@ class MixCommandParser(object):
             return [add_tag_command, move_file_command]
         commands = []
         for audio_file in os.listdir(add_tag_args.source):
+            source_path = os.path.join(add_tag_args.source, audio_file)
             add_tag_command = AddTagCommand(self._configuration_provider, self._tagger)
-            add_tag_command.source = os.path.join(add_tag_args.source, audio_file)
+            add_tag_command.source = source_path
             commands.append(add_tag_command)
             move_file_command = MoveAudioFileCommand(self._configuration_provider)
+            move_file_command.source = source_path
             commands.append(move_file_command)
         return commands
 
