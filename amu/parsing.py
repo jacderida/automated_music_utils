0f3e722fb80d0d969f7cf7283d69a95d2e037a7f
Chris O'Neil <chris.oneil@gmail.com>
2015-10-11 02:21:07 +0100

    Case for title set correctly

--- amu/parsing.py
+++ amu/parsing.py
@@ -790,6 +790,7 @@ class AddTagCommandArgs(object):
         command_args.source = AddTagCommandArgs._get_value_from_args(args.source)
         command_args.artist = AddTagCommandArgs._get_value_from_args(args.artist)
         command_args.album = AddTagCommandArgs._get_value_from_args(args.album)
+        command_args.title = AddTagCommandArgs._get_value_from_args(args.title)
         return command_args
 
     @staticmethod
